ec536dae42cd6f5f2b93d97202d4fb31acb96201
Oleksii Mykhailetskyi <melsmels1526@gmail.com>
2020-11-14 06:35:43 +0200

    add burger menu

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 mobileMenuBtn = document.querySelector(".nav-mobile-menu")
 navigation = document.querySelector(".navigation")
 
-mobileMenuBtn.addEventListener("click",function (){
+mobileMenuBtn.addEventListener("click", function () {
 
     mobileMenuBtn.classList.toggle("active-menu")
     if (mobileMenuBtn.classList.toggle("active-menu")) {
         navigation.classList.remove("active-menu")
-    }if (mobileMenuBtn.classList.toggle("active-menu")) {
+    }
+    if (mobileMenuBtn.classList.toggle("active-menu")) {
         navigation.classList.add("active-menu")
     }
 })
