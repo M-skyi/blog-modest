86f193884a03c51dae05b28c3d5622a0bbb77ff1
Oleksii Mykhailetskyi <melsmels1526@gmail.com>
2020-11-15 06:44:50 +0200

    created window modal

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ mobileMenuBtn.addEventListener("click", function () {
         navigationMobile.classList.remove("active-menu")
     }
     if (mobileMenuBtn.classList.toggle("active-menu")) {
-        navigationMobile.classList.add("active-menu") 
+        navigationMobile.classList.add("active-menu")
     }
+})
+let modalBtnClose = document.querySelector(".modal-Btn-close")
+let modalWindow = document.querySelector(".modal")
+let pageScroll = document.querySelector("body").scrollHeight;
+let middlePage = pageScroll / 2;
+
+window.addEventListener("scroll", scrollPage);
+
+function scrollPage() {
+    if (window.pageYOffset >= middlePage) {
+        modalWindow.classList.add("show-modal");
+    }
+}
+modalBtnClose.addEventListener("click", function () {
+    modalWindow.classList.remove("show-modal")
+    window.removeEventListener("scroll", scrollPage)
 })
