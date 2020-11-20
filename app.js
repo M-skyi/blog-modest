eb1f1c1b0306a3261038d24ba5406c7b49a281eb
Oleksii Mykhailetskyi <melsmels1526@gmail.com>
2020-11-20 13:44:51 +0200

    add script close modal

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mobileMenuBtn.addEventListener("click", function () {
         navigationMobile.classList.add("active-menu")
     }
 })
-let modalBtnClose = document.querySelector(".modal-Btn-close")
+let modalBtnClose = document.querySelector(".modal-btn-close")
 let modalWindow = document.querySelector(".modal")
 let pageScroll = document.querySelector("body").scrollHeight;
 let middlePage = pageScroll / 2;
@@ -23,7 +23,17 @@ function scrollPage() {
         modalWindow.classList.add("show-modal");
     }
 }
-modalBtnClose.addEventListener("click", function () {
+modalBtnClose.addEventListener("click", closeModal)
+
+function closeModal() {
     modalWindow.classList.remove("show-modal")
     window.removeEventListener("scroll", scrollPage)
+}
+
+modalWindow.addEventListener("click", function (event) {
+
+    if (event.target === modalWindow) {
+        closeModal()
+    }
+
 })
